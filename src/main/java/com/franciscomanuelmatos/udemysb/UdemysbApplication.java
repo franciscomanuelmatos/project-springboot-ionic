ca994f12d9070745227d44ac57d844d3ac5a2483
franciscomanuelmatos <franciscomanuelmatos@gmail.com>
2018-04-07 21:44:44 +0100

    Project created

---
+++ src/main/java/com/franciscomanuelmatos/udemysb/UdemysbApplication.java
@@ -0,0 +1,12 @@
+package com.franciscomanuelmatos.udemysb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemysbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemysbApplication.class, args);
+	}
+}
