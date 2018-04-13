804efe0d43734c094851ef129592360316523ed4
franciscomanuelmatos <franciscomanuelmatos@gmail.com>
2018-04-13 18:51:46 +0100

    Created domain classes City and State

--- src/main/java/com/franciscomanuelmatos/udemysb/UdemysbApplication.java
+++ src/main/java/com/franciscomanuelmatos/udemysb/UdemysbApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.franciscomanuelmatos.udemysb.domain.Category;
+import com.franciscomanuelmatos.udemysb.domain.City;
 import com.franciscomanuelmatos.udemysb.domain.Product;
+import com.franciscomanuelmatos.udemysb.domain.State;
 import com.franciscomanuelmatos.udemysb.repositories.CategoryRepository;
+import com.franciscomanuelmatos.udemysb.repositories.CityRepository;
 import com.franciscomanuelmatos.udemysb.repositories.ProductRepository;
+import com.franciscomanuelmatos.udemysb.repositories.StateRepository;
 
 @SpringBootApplication
 public class UdemysbApplication implements CommandLineRunner {
@@ -20,6 +24,12 @@ public class UdemysbApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private CityRepository cityRepo;
+	
+	@Autowired
+	private StateRepository stateRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(UdemysbApplication.class, args);
@@ -41,7 +51,19 @@ public class UdemysbApplication implements CommandLineRunner {
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1));
 		
+		State s1 = new State(null, "Minas Gerais");
+		State s2 = new State(null, "São Paulo");
+		
+		City c1 = new City(null, "Uberlândia", s1);
+		City c2 = new City(null, "São Paulo", s2);
+		City c3 = new City(null, "Campinas", s2);
+		
+		s1.getCities().addAll(Arrays.asList(c1));
+		s2.getCities().addAll(Arrays.asList(c2, c3));
+		
 		categoryRepo.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		stateRepo.saveAll(Arrays.asList(s1, s2));
+		cityRepo.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
---
+++ src/main/java/com/franciscomanuelmatos/udemysb/repositories/StateRepository.java
@@ -0,0 +1,11 @@
+package com.franciscomanuelmatos.udemysb.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.franciscomanuelmatos.udemysb.domain.State;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+}
